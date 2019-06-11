cae6c1b133ce7e0a95a0fc27238d8319edf86109
Steve <smather@astev.com>
2019-06-11 16:17:57 +1000

    initial commit

---
+++ src/components/TodoItem.vue
@@ -0,0 +1,45 @@
+<template>
+  <!-- bind class (is-complete) to conditional, if completed is true -->
+  <div class="todo-item" v-bind:class="{'is-complete':todo.completed}">
+    <!-- on event change call the method markComplete -->
+    <p>
+      <input type="checkbox" v-on:change="markComplete">
+      {{todo.title}}
+      <button @click="$emit('del-todo', todo.id)" class="del">x</button>
+    </p>
+  </div>
+</template>
+<script>
+export default {
+  name: "TodoItem",
+  // get the properties passed from the Todo component and store in props
+  props: ["todo"],
+  methods: {
+    markComplete() {
+      this.todo.completed = !this.todo.completed;
+    }
+  }
+};
+</script>
+
+<style scoped>
+.todo-item {
+  background: #f4f4f4;
+  padding: 10px;
+  border-bottom: 1px #ccc dotted;
+}
+
+.is-complete {
+  text-decoration: line-through;
+}
+
+.del {
+  background: #ff0000;
+  color: #fff;
+  border: none;
+  padding: 5px 9px;
+  border-radius: 50%;
+  cursor: pointer;
+  float: right;
+}
+</style>
---
+++ src/components/Todos.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <!-- loop through the array passed from the app component -->
+    <div v-bind:key="todo.id" v-for="todo in todos">
+      <!-- write out the TodoItem tag and send  data (todo object) to the TodoItem component -->
+      <TodoItem v-bind:todo="todo" v-on:del-todo="$emit('del-todo', todo.id)"/>
+    </div>
+  </div>
+</template>
+<script>
+// import the TodoItem component
+import TodoItem from "./TodoItem.vue";
+export default {
+  name: "Todos",
+  components: {
+    // define the TodoItem component here
+    TodoItem
+  },
+  // return the props back to the app component as an array
+  props: ["todos"]
+};
+</script>
+<style scoped>
+</style>
